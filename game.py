482a90140fea91dafb52a07eae6be4740c73b821
Aferu Minas <seyfeminas@gmail.com>
2021-08-12 13:05:09 -0700

    Created skeleton for game class

---
+++ game.py
@@ -0,0 +1,37 @@
+
+class Game:
+
+
+   def __init__(self):
+    #Has A
+    self.player_one = Human()
+    self.player_two = None
+   # Can Do
+   def run_game(self):
+    #Setup Phase
+    #Welcome
+    #Display rules, what beats what?
+    #Determine Game Type-Single player or multi?
+    #Creat player based on game type
+    #(Not required) Ask players their name?
+
+
+    #Game rounds - Repeat until one player has two points
+    #Player one choose a gesture
+    #Prompt user to enter gesture
+    #Save choise somewhere
+    #Player two chooses a gesture
+    #Compare gustures
+    #  - Winer gets a point
+    #  - No point if tie round
+    # Display winner of round
+
+
+    #End game
+    #Display winner of game
+    #(Not required) Ask if they want to play again
+    pass
+
+   def determine_game_type(self):
+    # Set self.player_two to human or AI
+    pass
